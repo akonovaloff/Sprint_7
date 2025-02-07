29b8acd37d911733d8c71b2cf04065102a55ec7b
Andrei Konovalov <a.konovaloff13@yandex.com>
2025-02-07 15:09:27 +0300

    Updated conftest.py

--- conftest.py
+++ conftest.py
@@ -1,18 +1,22 @@
 import pytest
 from scooter_api.scooter_api import ApiClient
-
+from faker import Faker
 
 @pytest.fixture(scope='session', autouse=True)
-def client() -> ApiClient:
-    return ApiClient()
+def client():
+    client = ApiClient()
+    return client
 
-@pytest.fixture()
-def delete_courier_before_test(self, client):
-    for courier_data in self.COURIER_DATA_REGISTER_SUCCESS:
-        response = client.send_login_to_account_request(courier_login=courier_data["courier_login"],
-                                                        courier_password=courier_data["courier_password"])
-        if response.status_code == client.LOGIN_SUCCESS_CODE:
-            """Удалить аккаунт перед тестом"""
-            courier_id = str(response.json()["id"])
-            delete_resp = client.send_delete_courier_request(courier_id)
-            assert delete_resp.status_code == client.DELETE_SUCCESS_CODE
+@pytest.fixture
+def test_user():
+    faker = Faker()
+    created_user = {"login": faker.email(),
+            "password": faker.password(10),
+            "first_name": faker.first_name()}
+    yield created_user
+
+@pytest.fixture
+def registered_user(client, test_user):
+    client.register(test_user)
+    yield test_user
+    client.delete(test_user)
